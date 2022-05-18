cf4d095d61841b9d2bf404f30bbb1c161eb25e28
justin.hobbs <justinhobbs@fastmail.com>
2022-05-18 08:13:31 -0500

    commit tree 39993142c1eecb856f145c48aafab189d60c2e01

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const allCats = [...cats, name];
+    return allCats;
+}
+
+function prependCat(name) {
+    const allCats = [name, ...cats];
+    return allCats;
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, 2); 
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1); 
+    return removeFirstCat;
+}
